08932eeb3bcd442aeffe96eef46e90553688d1db
Essjay01 <joboluwaseun28@gmail.com>
2024-02-06 11:19:08 +0100

    feat: added featured properties function

--- route/propertyRouter.js
+++ route/propertyRouter.js
@@ -6,10 +6,12 @@ const {
     handleDeleteProperty,
     handleEditProperty,
     getASingleProperty,
+    handleFeaturedProperties
 } = require("../contollers/propertyContoller")
 
 router.route('/').get(handleGetAllProperties).post(handleAddProperty)
 router.get('/recent',  handleGetRecentProperties)
+router.get('/featured',  handleFeaturedProperties)
 
 router.route("/:propertyId").get(getASingleProperty).patch(handleEditProperty).delete(handleDeleteProperty)
 
